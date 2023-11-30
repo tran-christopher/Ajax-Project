f64f038b974eadbc624a1bc7b097a345ee026f65
Christopher Tran <tran.christopher09@gmail.com>
2023-11-30 19:36:52 +0000

    Added eventListener for search bars and returns recipes #1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
-const $searchBarOne = document.querySelector('.search-one');
+const $searchBarOneInput = document.querySelector('.search-one');
+const $searchBarTwoInput = document.querySelector('.search-two');
+const $handleSearchOne = document.querySelector('.form-one');
+const $handleSearchTwo = document.querySelector('.form-two');
 
-function getPokemonData(name) {
+$handleSearchOne.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const criteria = $searchBarOneInput.value;
+  getRecipes(criteria);
+  $searchBarOneInput.value = null;
+});
+
+$handleSearchTwo.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const criteria = $searchBarOneInput.value;
+  getRecipes(criteria);
+  $searchBarTwoInput.value = null;
+});
+
+function getRecipes(parameter) {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + name);
+  xhr.open(
+    'GET',
+    `https://api.edamam.com/api/recipes/v2?type=public&q=${parameter}&app_id=b093ed76&app_key=9d739d793a989a61b52ed12591b6a75a`,
+  );
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     console.log(xhr.status);
