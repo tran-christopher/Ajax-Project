09f767a772ed6354900cba1cfba3eea6b4a8ac5e
Christopher Tran <tran.christopher09@gmail.com>
2023-11-30 18:00:54 +0000

    Syntax fixes #1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const $searchBarOne = document.querySelector('.search-one');
+
+function getPokemonData(name) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://pokeapi.co/api/v2/pokemon/' + name);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log(xhr.status);
+    console.log(xhr.response);
+  });
+  xhr.send();
+}
